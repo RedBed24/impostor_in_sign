1f7747d987d30129a29d50e353887447368afd60
elena-17 <95474265+elena-17@users.noreply.github.com>
2024-11-13 20:24:41 +0100

    update id

--- database/create_database.py
+++ database/create_database.py
@@ -1,11 +1,11 @@
 from pymongo import MongoClient
-import pickle 
 import os
 from dotenv import load_dotenv
 import string
 import pandas as pd
-import pickle
 import logging
+from hashlib import md5
+
 
 logging.basicConfig(level=logging.INFO)
 
@@ -24,25 +24,27 @@ def main():
 
     # Create collection
     raw_images_collection = db["raw_images"]
-
+    raw_images_collection.drop() #si existe la borra
     count = raw_images_collection.count_documents({})
     logging.info(f"Documents in the collection: {count}")
+    # Delete collection
 
-    if count == 0:
-        # Download the dataset
-        logging.info("Downloading the dataset")
-        df = pd.read_parquet("hf://datasets/Marxulia/asl_sign_languages_alphabets_v03/data/train-00000-of-00001.parquet")
+    # Download the dataset
+    logging.info("Downloading the dataset")
+    df = pd.read_parquet("hf://datasets/Marxulia/asl_sign_languages_alphabets_v03/data/train-00000-of-00001.parquet")
 
-        # Create a mapping from numbers to letters
-        label_mapping = {i: letter for i, letter in enumerate(string.ascii_uppercase)}
+    df['_id'] = df['image.bytes'].apply(lambda x: md5(x).hexdigest())
+    # Create a mapping from numbers to letters
+    label_mapping = {i: letter for i, letter in enumerate(string.ascii_uppercase)}
 
-        df['label'] = df['label'].astype(int)
-        df['label'] = df['label'].map(label_mapping)
+    df['label'] = df['label'].astype(int)
+    df['label'] = df['label'].map(label_mapping)
 
-        data_to_insert = df.to_dict(orient="records")
-        result = raw_images_collection.insert_many(data_to_insert)
+    data_to_insert = df.to_dict(orient="records")
 
-        logging.info(f"Inserted {len(result.inserted_ids)} elements")
+    result = raw_images_collection.insert_many(data_to_insert)
+
+    logging.info(f"Inserted {len(result.inserted_ids)} elements")
 
 
 if __name__ == "__main__":
