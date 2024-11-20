b3cddf8982a09ca926792062a9ea1c8fc2da27eb
Samuel Espejo Gil <69651116+RedBed24@users.noreply.github.com>
2024-11-20 23:29:36 +0100

    feat: add base web template

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,12 @@
+import { Container, Title } from '@mantine/core';
+
+export const App: React.FC = () => {
+
+  return (
+    <>
+      <Container>
+        <Title>Impostor in Sign</Title>
+      </Container>
+    </>
+  );
+};
