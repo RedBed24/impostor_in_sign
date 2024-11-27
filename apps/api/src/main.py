ddb11d19074cab50c00380ab16547dd580db0364
elena-17 <95474265+elena-17@users.noreply.github.com>
2024-11-27 16:22:26 +0100

    feat: authentication and new endpoint

--- apps/api/src/main.py
+++ apps/api/src/main.py
@@ -1,14 +1,34 @@
 from hashlib import md5
+from typing import Annotated
+
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 from db_connector import MongoDBConnector
-from fastapi import FastAPI, File, Form, UploadFile
+from fastapi import Depends, FastAPI, File, Form, HTTPException, UploadFile, status
 from fastapi.responses import Response
 from pydantic import BaseModel
+from .user_utils import authenticate_user
+from .auth_utils import create_access_token, Token, get_current_user
 
 app = FastAPI(
     on_shutdown=[lambda: MongoDBConnector().close_connection()],
 )
 
+@app.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> Token:
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token = create_access_token(
+        data={"sub": user.username}
+    )
+    return Token(access_token=access_token, token_type="bearer")
 
 @app.get("/api/img")
 async def get_imgs(limit: int = 5) -> dict:
@@ -35,9 +55,15 @@ async def get_img(img_id: str) -> dict:
     img = db.raw_images.find_one({"_id": img_id})
     return Response(content=img["image.bytes"], media_type="image/jpeg")
 
+@app.get("/api/img/label/{label}")
+async def get_img_by_label(label: str) -> dict:
+    db = MongoDBConnector.get_db()
+    imgs = list(db.raw_images.find({"label": label}))
+    imgs = list(map(lambda x: {"label": x["label"], "id": x["_id"]}, imgs))
+    return {"images": imgs}
 
 @app.delete("/api/img/{img_id}")
-async def delete_img(img_id: str) -> dict:
+async def delete_img(img_id: str, current_user:Annotated[str, Depends(get_current_user)]) -> dict:
     db = MongoDBConnector.get_db()
     db.raw_images.delete_one({"_id": img_id})
     return {"status": "deleted"}
@@ -64,3 +90,7 @@ async def post_img(img_id: str, new_label: Label) -> dict:
     db = MongoDBConnector.get_db()
     db.raw_images.update_one({"_id": img_id}, {"$set": new_label.model_dump()})
     return {"status": "updated"}
+
+@app.get("/iamalive")
+async def iamalive() -> dict:
+    return {"status": "ok"}
