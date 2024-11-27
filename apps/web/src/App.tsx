40fa565895c388a56ce682de8cca1ee6d4c5a507
Samuel Espejo Gil <69651116+RedBed24@users.noreply.github.com>
2024-11-27 17:42:01 +0100

    feat: add base game-home

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,11 +1,23 @@
-import { Container, Title } from '@mantine/core';
+import { Link, Route, Switch } from 'wouter';
+import { Button, Container } from '@mantine/core';
+import { GameHome } from './pages/game/game-home';
 
 export const App: React.FC = () => {
 
   return (
     <>
       <Container>
-        <Title>Impostor in Sign</Title>
+        <Link href='/game'>
+          <Button>Go to Game</Button>
+        </Link>
+        <Link href='/dashboard'>
+          <Button>Dashboard</Button>
+        </Link>
+
+        <Switch>
+          <Route path="/game" component={GameHome} />
+          <Route path="/dasboard" />
+        </Switch>
       </Container>
     </>
   );
---
+++ apps/web/src/pages/game/game-home.tsx
@@ -0,0 +1,13 @@
+import { Container, Title } from '@mantine/core';
+
+export const GameHome: React.FC = () => {
+
+  return (
+    <>
+      <Container>
+        <Title>Impostor in Sign</Title>
+      </Container>
+    </>
+  );
+};
+
