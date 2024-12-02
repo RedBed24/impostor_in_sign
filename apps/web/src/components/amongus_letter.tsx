bc1a9ba497c3b6ea4bd3c409aa3f769c1ea4793d
elena-17 <95474265+elena-17@users.noreply.github.com>
2024-12-02 19:28:29 +0100

    feat: add vertical movement to player

--- apps/web/src/components/amongus_letter.tsx
+++ apps/web/src/components/amongus_letter.tsx
@@ -1,58 +1,185 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Box, Image, Stack } from '@mantine/core';
+import { useSpring, animated } from '@react-spring/web';
 
 interface AmongusLetterProps {
   prediction: string | null;
+  speed: number;
+  isPaused: boolean | null;
 }
 
-const AmongusLetter: React.FC<AmongusLetterProps> = ({prediction}) => {
+const AmongusLetter: React.FC<AmongusLetterProps> = ({ prediction, speed, isPaused = false }) => {
+  const [correct, setCorrect] = useState<boolean>(false);
   const [letter, setLetter] = useState<string>('');
   const [background, setBackground] = useState<string>('white');
   const [imageIndex, setImageIndex] = useState<number>(0);
+  const [position, setPosition] = useState<number>(-150); // Comienza fuera de la pantalla a la izquierda
+  const frameDuration = 100; // Duración de cada frame en ms
+  const [verticalPosition, setVerticalPosition] = useState<number>(0); // Posición vertical inicial
+  const [movingVertically, setMovingVertically] = useState<boolean>(false); // Control del movimiento vertical
+  const containerRef = useRef<HTMLDivElement>(null); // Referencia al contenedor
 
+
+  const calculateInitialVerticalPosition = () => {
+    if (containerRef.current) {
+      const rect = containerRef.current.getBoundingClientRect();
+      return rect.top;
+    }
+    return 0;
+  };
+
+
+  useEffect(() => {
+    setVerticalPosition(calculateInitialVerticalPosition());
+  }, []);
+
+  // Actualizamos los sprites a intervalos regulares
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setImageIndex((prevIndex) => (prevIndex % 12) + 1);
+    }, frameDuration);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    let animationFrame: number;
+
+    const reset = () => {
+      setCorrect(false);
+      generateRandomLetter();
+      setMovingVertically(false);
+      setVerticalPosition(calculateInitialVerticalPosition());
+    };
+
+    const animate = () => {
+      setPosition((prevPosition) => {
+
+        // Movimiento horizontal
+        const newPosition = prevPosition + speed;
+
+        // Iniciar el movimiento vertical en el medio de la pantalla
+        if (newPosition >= window.innerWidth * 0.5 && newPosition <= window.innerWidth * 0.70) {
+          setMovingVertically(true);
+        } else {
+          setMovingVertically(false);
+        }
+
+        // Calcular posición vertical como una parábola
+        if (movingVertically) {
+          const peakX = window.innerWidth * 0.7;
+          const height = -300; // Altura máxima de la parábola
+          const widthFactor = (window.innerWidth * 0.20);
+          if (correct) {
+            const parabola = -height * Math.pow((newPosition - peakX) / widthFactor, 2) + height + 20;
+            setVerticalPosition(parabola);
+          } else {
+            console.log(verticalPosition);
+            setBackground('red');
+            const straight = verticalPosition + speed;
+            setVerticalPosition(straight);
+          }
+        }
+
+
+        if (newPosition > window.innerWidth - 100 || verticalPosition > 120) {
+          reset();
+          return -150; // Vuelve a la izquierda, fuera de la pantalla
+        }
+        return newPosition;
+      });
+
+      animationFrame = requestAnimationFrame(animate); // Continuar la animación
+    };
+
+    animationFrame = requestAnimationFrame(animate);
+
+    return () => cancelAnimationFrame(animationFrame); // Limpiar el frame al desmontar el componente
+  }, [speed, movingVertically, correct, verticalPosition]);
+
+  // Usar React Spring para animar el movimiento
+  const { left } = useSpring({
+    left: position,
+    top: verticalPosition,
+    config: { tension: 170, friction: 26 },
+  });
+
+  // Generar una letra aleatoria
   const generateRandomLetter = () => {
     const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     const randomLetter = letters[Math.floor(Math.random() * letters.length)];
     setLetter(randomLetter);
   };
 
-  useEffect(() => {
-    generateRandomLetter();
-  }, []);
+
 
   useEffect(() => {
+    generateRandomLetter();
+  }, []); // Solo genera una letra al principio
+
+  // Cambiar el fondo cuando se adivina la letra
+  useEffect(() => {
     if (prediction && letter === prediction) {
+      setCorrect(true);
       setBackground('lightgreen');
-      generateRandomLetter();
-    }
-    else {
+      generateRandomLetter(); 
+    } else {
       setBackground('white');
     }
   }, [prediction, letter]);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setImageIndex((prevIndex) => (prevIndex % 12) + 1);
-    }, 70);
-
-    return () => clearInterval(interval);
-  }, []);
-
   return (
-  <>
-  <Stack align='center' w={100} h={100} gap ="xs">
-    <Box pl={10} pr={10} bd="2px solid black" style={{background: `${background}` }} fz={20}>
-      {letter}
-    </Box>
-    <Image
-      src={`/src/assets/amongus/red/Walk000${imageIndex}.png`}
-      alt={`Letter ${letter}`}
-      height="auto"
-      width="auto"
-      m = {0} 
-    />
-  </Stack>
-  </> 
+    <>
+      <animated.div
+        style={{
+          position: 'absolute',
+          left: left, // Movimiento animado del personaje
+          top: verticalPosition, // Movimiento vertical controlado
+          opacity: position >= 0 ? 1 : 0, // Evita que sea visible cuando está fuera de la pantalla
+        }}
+      >
+        <Stack align="center" w={100} h={100} gap={0}>
+          {correct ? <Image
+            src={`/src/assets/ballon.png`}
+            alt={`ballon`}
+            top={0}
+            bottom={0}
+            height="75%"
+            width="10%"
+            fit="contain"
+            m={0}
+          /> : <Box
+            pl={10}
+            pr={10}
+            m ={4}
+            bd="2px solid black"
+            style={{ background: `${background}` }}
+            fz={30}
+          >
+            {letter}
+          </Box>}
+
+          {background === 'red' ? (
+            <Image
+            src={`/src/assets/amongus/red/idle.png`}
+            alt={`Other Image`}
+            height="auto"
+            width="auto"
+            m={0}
+          />
+          ) : (
+            <Image
+              src={`/src/assets/amongus/red/Walk000${imageIndex}.png`}
+              alt={`Letter ${letter}`}
+              height="auto"
+              width="auto"
+              m={0}
+            />
+            
+          )}
+        </Stack>
+      </animated.div>
+    </>
   );
 };
 
--- apps/web/src/pages/game/game-home.tsx
+++ apps/web/src/pages/game/game-home.tsx
@@ -1,12 +1,13 @@
-import { Container, Title, Text, Group, Box } from '@mantine/core';
-// import { Check, X } from 'lucide-react';
+import { Container, Title, Text, Group, Box, BackgroundImage, Grid, Button } from '@mantine/core';
+import { Pause, Play } from 'lucide-react';
+
 import Webcam from "react-webcam";
 import { useRef, useState, useEffect } from "react";
 import AmongusLetter from '../../components/amongus_letter';
 
 const videoConstraints = {
-  width: 300,
-  height: 300,
+  width: 200,
+  height: 200,
   facingMode: "user"
 };
 
@@ -14,6 +15,19 @@ export const GameHome: React.FC = () => {
   const webcamRef = useRef<Webcam>(null);
   const [prediction, setPrediction] = useState<string | null>(null);
   const [isCameraReady, setCameraReady] = useState(false);
+  
+  //test
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      setPrediction(event.key.toUpperCase());
+    };
+  
+    window.addEventListener('keydown', handleKeyDown);
+  
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
 
 
   const handleUserMedia = () => {
@@ -78,35 +92,46 @@ export const GameHome: React.FC = () => {
   /////////////////////////////////////////////////////////////////////////////////////////////////////////
   return (
     <>
-      <Container>
-        <Title>Impostor in Sign</Title>
-        <Text fz={35} m={30}>PREDICTION: {prediction}</Text>
-
-        <Group gap='xs'>
-          <Box
-            style={{
-              border: "5px solid #4a90e2",
-              borderRadius: "10px",
-              width: "310px",
-              height: "310px",
-              margin: "auto",
-              overflow: "hidden",
-            }}
-          >
-            <Webcam
-              audio={false}
-              ref={webcamRef}
-              screenshotFormat="image/jpeg"
-              videoConstraints={videoConstraints}
-              mirrored={true}
-              imageSmoothing={true}
-              onUserMedia={handleUserMedia}
-            />
-          </Box>
-          <AmongusLetter prediction={prediction} />
-
-        </Group>
-      </Container>
+      <BackgroundImage
+        src="/src/assets/fondo_bloques.png"
+        style={{
+          width: '100vw',
+          height: '95vh',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          display: 'flex',
+          flexDirection: 'column'
+        }}
+      >
+        <Grid p={0}>
+          <Grid.Col span={12}>
+            <Group style={{ width: '100%' }}>
+              <Text fz={35} m={60} c='white'>PREDICTION: {prediction}</Text>
+              <Box ml={60} 
+              style={{
+                height: "210px",
+                border: "5px solid #4a90e2",
+                borderRadius: "10px",
+              }}>
+              <Webcam
+                audio={false}
+                ref={webcamRef}
+                screenshotFormat="image/jpeg"
+                videoConstraints={videoConstraints}
+                mirrored={true}
+                imageSmoothing={true}
+                onUserMedia={handleUserMedia}
+              />
+              </Box>
+            </Group>
+          </Grid.Col>
+          
+            <Grid.Col style={{ position: 'absolute', top: '53%' }}>
+          <AmongusLetter prediction={prediction} speed={3} isPaused={true}/>
+          </Grid.Col>
+        </Grid>
+      </BackgroundImage>
     </>
   );
 };
