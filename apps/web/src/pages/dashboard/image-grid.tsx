26272f0b297a71006a436b5ec4d94de7af22dd87
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2024-12-09 13:19:07 +0100

    feat: add image upload

--- apps/web/src/pages/dashboard/image-grid.tsx
+++ apps/web/src/pages/dashboard/image-grid.tsx
@@ -19,7 +19,6 @@ export const ImageGrid: React.FC = () => {
         },
       });
       const data = await response.json();
-      console.log("token stored")
       localStorage.setItem('token', data.access_token);
     } catch (error) {
       console.error('Error getting token:', error);
@@ -40,11 +39,44 @@ export const ImageGrid: React.FC = () => {
     fetchImages();
   }, []);
 
+  const handleUploadImage = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (!file) return;
+
+    const formData = new FormData();
+    formData.append('file', file, file.name);
+    formData.append('label', "test");
+
+    try {
+      const token = localStorage.getItem('token');
+      const response = await fetch('/api/img/', {
+        method: 'PUT',
+        body: formData,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to upload image');
+      }
+    } catch (error) {
+      console.error('Error uploading image:', error);
+    }
+  };
+
   return (
     <>
       <Container>
         <Title>Dashboard</Title>
         <Button onClick={handleGetToken} >Get Token</Button>
+        <input
+          type="file"
+          onChange={handleUploadImage}
+          style={{ display: 'none' }}
+          id="upload-input"
+        />
+        <Button onClick={() => document.getElementById('upload-input')?.click()} >Upload Image</Button>
         <SimpleGrid cols={3} spacing="lg">
           {images.map((image, index) => (
             <PhotoCard key={index} imageID={image.id} />
