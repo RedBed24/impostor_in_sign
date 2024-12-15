efc3dad19a9353e7e1c4d6df853deda05f673029
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2024-12-15 16:41:31 +0100

    feat(api): add skip to get images

--- apps/api/src/main.py
+++ apps/api/src/main.py
@@ -31,9 +31,9 @@ async def login_for_access_token(
     return Token(access_token=access_token, token_type="bearer")
 
 @app.get("/api/img")
-async def get_imgs(limit: int = 5) -> dict:
+async def get_imgs(limit: int = 5, skip: int = 0) -> dict:
     db = MongoDBConnector.get_db()
-    imgs = list(db.raw_images.find({}).limit(limit))
+    imgs = db.raw_images.find({}).skip(skip).limit(limit)
     imgs = list(map(lambda x: {"label": x["label"], "id": x["_id"]}, imgs))
     return {"images": imgs}
 
